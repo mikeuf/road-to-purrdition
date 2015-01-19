11bd265792082f4f544f2030ac207009c7ac303d
Daniel McNaughton <danieljmcnaughton@gmail.com>
2015-01-19 00:31:05 -0500

    Moved the navbar code to the header file.

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,8 @@
 <?php
 include_once('include.php');
+if (!(isset($title))) {
+	$title = "Road To Purrdition";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,4 +27,30 @@ include_once('include.php');
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="description" content="">
 		<meta name="author" content="Daniel McNaughton">
-		
+
+		<title><?php echo $title ?></title>
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Road To Purrdition</a>
+				</div>
+				<div id="navbar" class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="http://roadtopurrdition.com">Home</a></li>
+						<li><a href="http://development.roadtopurrdition.com">Development</a></li>
+						<li><a href="http://caroline.roadtopurrdition.com">Caroline</a></li>
+						<li><a href="http://daniel.roadtopurrdition.com">Daniel</a></li>
+						<li><a href="http://jackie.roadtopurrdition.com">Jacqueline</a></li>
+						<li><a href="http://mike.roadtopurrdition.com">Mike</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
