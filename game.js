a6a34b0d6927e74df2ebfe532a93a82bb99ea1ea
Daniel McNaughton <danieljmcnaughton@gmail.com>
2015-01-19 01:39:42 -0500

    Added "Coming Soon" to the phaser panel.

--- game.js
+++ game.js
@@ -3,8 +3,10 @@ function create() {
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5 , 0.5);
 	var style = { font: "65px Arial", fill: "#ffffff", align: "center" };
-		var text = game.add.text(game.world.centerX, game.world.centerY, "Hello World!", style);
-		text.anchor.set(0.5, 3);
+	var txtHelloWorld = game.add.text(game.world.centerX, game.world.centerY, "Hello World!", style);
+	txtHelloWorld.anchor.set(0.5, 3);
+	var txtComingSoon = game.add.text(game.world.centerX, game.world.centerY, "Coming Soon!", style);
+	txtComingSoon.anchor.set(0.5, -2.5);
 	}
 function preload () {
     game.load.image('logo', 'resources/img/phaser.png');
