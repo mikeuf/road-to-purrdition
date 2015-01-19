e5be47cae5935b9c802ceb4d09b5720c1257990b
Daniel McNaughton <danieljmcnaughton@gmail.com>
2015-01-19 01:16:13 -0500

    Attempting to fix the misalignment on the index.php page.

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-panel', { preload: preload, create: create });
 function create() {
     var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
     logo.anchor.setTo(0.5 , 0.5);
