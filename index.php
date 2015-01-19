c146e4da11ad5eca4bd3acda984fff092611aeef
Daniel McNaughton <danieljmcnaughton@gmail.com>
2015-01-18 19:30:39 -0500

    Updated landing page.

--- index.php
+++ index.php
@@ -4,8 +4,38 @@ include_once('includes/header.php');
 	<title>Road to Purrdition</title>
 </head>
 <body>
-<h1>Hello World!</h1>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Road To Purrdition</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="http://development.roadtopurrdition.com">Development</a></li>
+            <li><a href="http://caroline.roadtopurrdition.com">Caroline</a></li>
+            <li><a href="http://daniel.roadtopurrdition.com">Daniel</a></li>
+            <li><a href="http://jackie.roadtopurrdition.com">Jacqueline</a></li>
+            <li><a href="http://mike.roadtopurrdition.com">Mike</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Road To Purrdition - Landing Page</h1>
+        <p class="lead">This is the main landing page for Road To Purrdition. This page will host the current build of the <code>master</code> branch.</p>
+      </div>
+
+    </div><!-- /.container -->
 
 <?php
 include_once('includes/footer.php');
