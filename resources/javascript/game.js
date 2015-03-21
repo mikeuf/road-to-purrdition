3c995c6806d9e82e8e91d1933c62c803472f7898
Mike Black <mpb11g@my.fsu.edu>
2015-03-21 18:38:50 -0400

    adjusting what I think is the framerate

--- resources/javascript/game.js
+++ resources/javascript/game.js
@@ -66,7 +66,7 @@ game.add.sprite(0, 0, 'springBackground');
     //  player.animations.add('left', [2, 1], 5, true);
      //  player.animations.add('right', [3, 4], 5, true);
 
-     player.animations.add('left', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], 0, true);
+     player.animations.add('left', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], 5, true);
 
      player.animations.add('right', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], 0, true);
       cabbages = game.add.group();
