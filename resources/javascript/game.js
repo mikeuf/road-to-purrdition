e427bba59b3a14e7138b831e1c364dab567334f2
Mike Black <mpb11g@my.fsu.edu>
2015-03-29 13:20:20 -0400

    re-pushing main menu changes, though I thought I already did yesterday

--- resources/javascript/game.js
+++ resources/javascript/game.js
@@ -1,63 +1,70 @@
 
+/*
+This is the main menu for the game. You click on one of several buttons to load the relevant item. The actions temporarily implemented for now as just text, but will eventually load actual parts of the game.
+	
+Date Created March 28
+Author Mike Black
+*/
 
-
+// Loads the main game engine
     var game = new Phaser.Game(1024, 600, Phaser.AUTO, 'phaser-panel', { preload: preload, create: create, update: update });
 
-    function preload() {
-	
+
+// loads the sprites and images
+
+    function preload() 
+{
+
+// background was drawn by Jackie	
 	game.load.image('mainMenuBackground','assets/title_screen.png');
-    game.load.image('ground', 'assets/platform.png');
 
-
+// buttons are actually spritesheets
     game.load.spritesheet('buttonPlayGame', 'assets/buttonPlayGame_72.png',161,58);
     game.load.spritesheet('buttonInstructions', 'assets/buttonInstructions_72.png',167,58);
     game.load.spritesheet('buttonIntroduction', 'assets/buttonIntroduction_72.png', 172,56);
     game.load.spritesheet('buttonSettings', 'assets/buttonSettings_72.png',128,58);
-    game.load.spritesheet('buttonCredits', 'assets/buttonCredits_72.png',112,55);
-        
+    game.load.spritesheet('buttonCredits', 'assets/buttonCredits_72.png',112,55);    
     
-    }
+}
 
 
-// Global variables
+// Global variables -- some not yet used
 
-var buttonPlayGame;
-var buttonInstructions;
-var buttonIntroduction;
-var buttonSettings;
-var buttonCredits;
-var background;
-var buttonText;
-var muteFlag = 0;
-var player;
-var platforms;
-var cursors;
-var music;
+	var buttonPlayGame;
+	var buttonInstructions;
+	var buttonIntroduction;
+	var buttonSettings;
+	var buttonCredits;
+	var background;
+	var buttonText;
+	var muteFlag = 0;
+	var player;
+	var platforms;
+	var cursors;
+	var music;
 
-var NUM_OF_CABBAGES = 12;
 
-var prevDate = new Date();
-var prevMs = prevDate.getMilliseconds();
-
+// this function loads the various objects
 function create()
 {
 
-
+// set background color
     game.stage.backgroundColor = '#fff';
+    
+// loads text field that will be uploaded by button clicks (temporary)
     buttonText = game.add.text(50, 50, 'Click a button', { fontSize: '64px', fill: '#000' });
 
+// load background
     background = game.add.sprite(0, 0, 'mainMenuBackground');
-  buttonPlayGame = game.add.button(game.world.centerX - 50, 50, 'buttonPlayGame', actionButtonPlayGame, this, 1, 0);
-  buttonInstructions = game.add.button(game.world.centerX - 50, 110, 'buttonInstructions', actionButtonInstructions, this, 1, 0);
-  buttonIntroduction = game.add.button(game.world.centerX - 50, 170, 'buttonIntroduction', actionButtonIntroduction, this, 1, 0);
-  buttonSettings = game.add.button(game.world.centerX - 50, 230, 'buttonSettings', actionButtonSettings, this, 1, 0);
+    
+// load buttons 
+	buttonPlayGame = game.add.button(game.world.centerX - 50, 50, 'buttonPlayGame', actionButtonPlayGame, this, 1, 0);
+	buttonInstructions = game.add.button(game.world.centerX - 50, 110, 'buttonInstructions', actionButtonInstructions, this, 1, 0);
+	buttonIntroduction = game.add.button(game.world.centerX - 50, 170, 'buttonIntroduction', actionButtonIntroduction, this, 1, 0);
+	buttonSettings = game.add.button(game.world.centerX - 50, 230, 'buttonSettings', actionButtonSettings, this, 1, 0);
     buttonCredits = game.add.button(game.world.centerX - 50, 290, 'buttonCredits', actionButtonCredits, this, 1, 0);
 
-
-    buttonPlayGame.onInputOver.add(over, this);
-    buttonPlayGame.onInputOut.add(out, this);
-    buttonPlayGame.onInputUp.add(up, this);
-
+// button actions
 
 function actionButtonPlayGame() 
 {
@@ -84,13 +91,12 @@ function actionButtonCredits()
   buttonText.text = "Credits clicked"
 }
 
-    } // end create()?
+} // end create()
     
-    function update()
-    {
-	    
-	    
-    }
+function update()
+{
+// not yet used, but I am leaving this here because the game engine is expecting this function    	    
+} // end update()
 
   
     
